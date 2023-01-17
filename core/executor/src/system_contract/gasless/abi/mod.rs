aba5ccabd466bd952a6f46330127da4f884c4a68
wenyuanhust <wenyuanhust@gmail.com>
2023-01-17 21:55:35 +0800

    feat: gasless sync for tx pool  & execution

--- core/executor/src/system_contract/gasless/abi/mod.rs
+++ core/executor/src/system_contract/gasless/abi/mod.rs
@@ -1,1 +1,5 @@
 pub mod sponsor_whitelist_control_abi;
+
+use crate::system_contract::system_contract_address;
+use protocol::types::H160;
+pub const GASLESS_ADDRESS: H160 = system_contract_address(0x1);
